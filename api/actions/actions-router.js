17f2d6d1e4ad355d66d70633a5e98ad20bad8bbd
Amanda Burtness <amandaburtness@gmail.com>
2022-02-20 21:47:17 -0600

    completed sprint

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,54 @@
 // Write your "actions" router here!
+const Actions = require("./actions-model")
+const express = require("express")
+const router = express.Router()
+const {validateActionId, validateAction} = require("./actions-middlware")
+
+router.get('/', async (req, res) => {
+  try{
+    const actions = await Actions.get()
+    res.status(200).json(actions)
+  } catch(err) {
+    res.status(500).json({message: err.message})
+  }
+})
+
+router.get("/:id", validateActionId, async (req, res) => {
+  try{
+    res.status(200).json(req.action)
+  } catch(err){
+    res.status(500).json({message: err.message})
+  }
+})
+
+router.post("/", async (req, res) => {
+  try{
+    const newAction = await Actions.insert(req.body)
+    console.log(newAction)
+    res.status(201).json(newAction)
+  } catch{
+    res.status(500).json({message: err.message})
+  }
+})
+
+router.put("/:id", validateActionId, validateAction, async (req, res) => {
+  console.log(req.params.id)
+  const updatedAction = await Actions.update(req.params.id, {
+    project_id: req.project_id,
+    description: req.description,
+    notes: req.notes,
+    completed: req.completed
+    })
+  res.status(200).json(updatedAction)
+})
+
+router.delete("/:id", validateActionId, async (req, res) => {
+  try{
+    await Actions.remove(req.params.id)
+    res.json(res.Action)
+  } catch(err){
+    res.status(500).json({message: err.message})
+  }
+})
+
+module.exports = router  
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,80 @@
 // Write your "projects" router here!
+const Projects = require('./projects-model')
+const express = require("express")
+const router = express.Router()
+const {validateProjectId, validateProject} = require("./projects-middleware")
+
+router.get('/', (req, res) => {
+  Projects.get()
+  .then(project => {
+    res.status(200).json(project)
+  })
+  .catch(err => {
+    res.status(500).json({message: 'The posts information could not be retrieved'})
+  })
+})
+
+router.get("/:id", validateProjectId, (req,res) => {
+  try{
+    res.status(200).json(req.params)
+  } catch(err){
+    res.status(500).json({message: err.message})
+  }
+})
+
+router.post('/', (req, res) => {
+  const newProject = req.body
+  Projects.insert(newProject)
+    .then(project => {
+      res.status(201).json(newProject)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding the project',
+      })
+    })
+})
+
+router.put("/:id", validateProjectId, validateProject, (req,res) => {
+  const {name, description, completed} = req.body
+  if(!name || !description, !completed){
+    res.status(400).json({message: 'Project ID does not exist'})
+  } else{
+    Projects.update(req.params.id, req.body)
+      .then(() => {
+        return Projects.get(req.params.id)
+      })
+      .then(project => {
+        res.json(project)
+      })
+      .catch(err => {
+        res.status(500).json({message: err.message})
+      })
+  }
+})
+
+router.delete("/:id", validateProjectId, async (req, res) => {
+  try{
+    await Projects.remove(req.params.id)
+    res.json(res.Projects)
+  } catch(err){
+    res.status(500).json({message: err.message})
+  }
+})
+
+router.get("/:id/actions", validateProjectId, async (req, res) => {
+  Projects.getProjectActions(req.params.id)
+    .then(actions => {
+      if(actions.length > 0){
+        res.status(200).json(actions)
+      } else {
+        res.status(404).json((actions))
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message})
+    })
+})
+
+module.exports = router  
